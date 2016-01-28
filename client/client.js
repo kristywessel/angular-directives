4364540a9efa8f84cd76e10e5ea80f832b0f09a8
Kristy Wessel <wessel.kristy@gmail.com>
2016-01-28 15:51:43 -0600

    finished with walkthrough

--- client/client.js
+++ client/client.js
@@ -1,5 +1,10 @@
 var app = angular.module('myApp', []);
 
 app.controller('MainController', ['$scope', function($scope){
-    $scope.message = "Hello Angular";
+    $scope.message = 'Hello User!';
+    $scope.end = 'money';
+    $scope.endTwo = '123';
+    $scope.thing = false;
+    $scope.names = ['Pizza', 'Unicorns', 'Robots'];
+    $scope.my = {favorite: 'Unicorns'};
 }]);
